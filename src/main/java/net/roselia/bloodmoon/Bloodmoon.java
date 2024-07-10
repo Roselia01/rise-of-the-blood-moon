f289e678940baa6f361e8cd1772bccb6eb14f1e1
Roselia01 <haileybrownlee850@gmail.com>
2024-07-10 13:05:49 -0400

    Initial commit

---
+++ src/main/java/net/roselia/bloodmoon/Bloodmoon.java
@@ -0,0 +1,16 @@
+package net.roselia.bloodmoon;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Bloodmoon implements ModInitializer {
+	public static String MOD_ID = "bloodmoon";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/roselia/bloodmoon/BloodmoonClient.java
@@ -0,0 +1,10 @@
+package net.roselia.bloodmoon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BloodmoonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
